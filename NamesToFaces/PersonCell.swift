01f4b809cc320f6f10f3042774d6f5dac4e4c143
My Nguyen <My@mys-macbook-pro.local>
2016-08-08 22:47:57 -0700

    Designing UICollectionView cells

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by My Nguyen on 8/8/16.
+//  Copyright © 2016 My Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+}
