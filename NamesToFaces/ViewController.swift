ba1e52c4b47280b93aeb97a9f98bd2429bc9846f
My Nguyen <My@mys-macbook-pro.local>
2016-08-09 13:44:32 -0700

    Importing photos with UIImagePickerController

--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: #selector(addNewPerson))
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +35,52 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         // collection view cell is typecast to a PersonCell
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCell
         return cell
+    }
+
+    // implementation for UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // dismiss the image picker if the user cancels it
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var newImage: UIImage
+        /// extract the image from the dictionary parameter
+        // info is a dictionary containing 2 keys: UIImagePickerControllerEditedImage and UIImagePickerControllerOriginalImage
+        // need to use optional typecasting (as?) to make sure the dictionary value extracted is of type UIImage
+        if let possibleImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            newImage = possibleImage
+        } else if let possibleImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            newImage = possibleImage
+        } else {
+            return
+        }
+
+        // generate a unique filename for the image
+        let imageName = NSUUID().UUIDString
+        let imagePath = getDocumentsDirectory().stringByAppendingPathComponent(imageName)
+        // convert UIImage to NSData (JPEG)
+        if let jpegData = UIImageJPEGRepresentation(newImage, 80) {
+            // save NSData to file
+            jpegData.writeToFile(imagePath, atomically: true)
+        }
+
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func getDocumentsDirectory() -> NSString {
+        let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = paths[0]
+        return documentsDirectory
+    }
+
+    func addNewPerson() {
+        let picker = UIImagePickerController()
+        // allow user to crop the picture
+        picker.allowsEditing = true
+        // the current class needs to conform to both UIImagePickerControllerDelegate and UINavigationControllerDelegate
+        picker.delegate = self
+        presentViewController(picker, animated: true, completion: nil)
     }
 }
 
